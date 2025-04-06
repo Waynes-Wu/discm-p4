24059b8866fb874a27c067f9cf5b992b45d5fee6
Adrian Yung Cheng <aids_yungcheng@dlsu.edu.ph>
2025-04-07 02:51:43 +0800

    Attempted to fix add courses, but still encountering bugs
     - to insert a course into the database, use this code:
    
    docker exec -it enrollsys-postgres-1 psql -U postgres -d enrollment_course -c "INSERT INTO courses (course_name, course_code, instructor_id, department, credits) VALUES ('Introduction to Programming 3', 'CCPROG3', 1, 'Department 1', 3.0);"

--- main-service/src/main/java/p4/mainservice/dto/ApiResponse.java
+++ main-service/src/main/java/p4/mainservice/dto/ApiResponse.java
@@ -1,19 +1,36 @@
 package p4.mainservice.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.time.LocalDateTime;
 
 @Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class ApiResponse<T> {
     private boolean success;
     private String message;
     private T data;
     private LocalDateTime timestamp;
+
+    public ApiResponse() {
+        this.timestamp = LocalDateTime.now();
+    }
+
+    public static <T> ApiResponse<T> success(T data) {
+        ApiResponse<T> response = new ApiResponse<>();
+        response.setSuccess(true);
+        response.setData(data);
+        return response;
+    }
+
+    public static <T> ApiResponse<T> success(T data, String message) {
+        ApiResponse<T> response = success(data);
+        response.setMessage(message);
+        return response;
+    }
+
+    public static <T> ApiResponse<T> error(String message) {
+        ApiResponse<T> response = new ApiResponse<>();
+        response.setSuccess(false);
+        response.setMessage(message);
+        return response;
+    }
 }
